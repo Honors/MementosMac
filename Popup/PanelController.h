1599383a50dba2a4eb9fdbc0b24e921eff6b7fe0
Matt Neary <neary.matt@gmail.com>
2012-04-01 22:05:33 -0400

    Initial commit

---
+++ Popup/PanelController.h
@@ -0,0 +1,41 @@
+#import "BackgroundView.h"
+#import "StatusItemView.h"
+#import <WebKit/WebKit.h>
+
+@class PanelController;
+
+@protocol PanelControllerDelegate <NSObject>
+
+@optional
+
+- (StatusItemView *)statusItemViewForPanelController:(PanelController *)controller;
+
+@end
+
+#pragma mark -
+
+@interface PanelController : NSWindowController <NSWindowDelegate>
+{
+    BOOL _hasActivePanel;
+    BackgroundView *_backgroundView;
+    id<PanelControllerDelegate> _delegate;
+    NSSearchField *_searchField;
+    NSTextField *_textField;
+    
+    IBOutlet WebView *wb;
+}
+
+@property (assign) IBOutlet BackgroundView *backgroundView;
+@property (assign) IBOutlet NSSearchField *searchField;
+@property (assign) IBOutlet NSTextField *textField;
+
+@property (nonatomic, assign) BOOL hasActivePanel;
+@property (nonatomic, readonly) id<PanelControllerDelegate> delegate;
+
+- (id)initWithDelegate:(id<PanelControllerDelegate>)delegate;
+
+- (void)openPanel;
+- (void)closePanel;
+- (NSRect)statusRectForWindow:(NSWindow *)window;
+
+@end
